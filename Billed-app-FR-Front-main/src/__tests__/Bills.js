08af5f219a2a103cc3eb92ad27e6441d2ffe36db
Usman <usman.mughal@outlook.fr>
2023-06-27 13:26:11 +0200

    test Bills clique

--- Billed-app-FR-Front-main/src/__tests__/Bills.js
+++ Billed-app-FR-Front-main/src/__tests__/Bills.js
@@ -2,16 +2,17 @@
  * @jest-environment jsdom
  */
 
-import {screen, waitFor} from "@testing-library/dom"
+import {prettyDOM, screen, waitFor, fireEvent} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 
 import mockStore from "../__mocks__/store"
 
-import { ROUTES_PATH } from "../constants/routes"
+import { ROUTES_PATH, ROUTES } from "../constants/routes"
 import { localStorageMock } from "../__mocks__/localStorage.js"
 
 import { bills } from "../fixtures/bills"
 import router from "../app/Router"
+import Bills from "../containers/Bills.js"
 
 jest.mock("../app/store", () => mockStore) // ?
 
@@ -42,6 +43,31 @@ describe("Given I am connected as an employee", () => {
     })
   })
 })
+
+describe('When I click on the eye icon', () => { 
+  test("It should render a modal", async () => {
+    $.fn.modal = jest.fn() // JQuery Error
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
+
+    const onNavigate = (pathname) => {
+      document.body.innerHTML = ROUTES({ pathname})
+    }
+
+    document.body.innerHTML = BillsUI({ data: bills.sort((a, b) => new Date(b.date) - new Date(a.date)) })
+
+    new Bills({
+      document, onNavigate, localStorage: window.localStorage
+    });
+
+    const eyeIcon = screen.getAllByTestId("icon-eye")[0];
+    fireEvent.click(eyeIcon);
+
+    expect(screen.getByText("Justificatif")).toBeTruthy();
+    expect(screen.getByAltText("Bill")).toBeTruthy();
+  })
+})
+
 
 // test d'intégration GET
 describe("Given I am connected as an employee", () => {
